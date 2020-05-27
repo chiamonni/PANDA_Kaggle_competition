8c0f26b29a055bcd2381d05be02ab4ff1b3a688f
mawanda-jun <giovanni.cavallin@outlook.com>
2020-05-27 10:11:38 +0200

    dataset modified for new data type

--- dataset.py
+++ dataset.py
@@ -12,16 +12,17 @@ import skimage.io
 import torchvision.transforms
 from PIL import Image
 import random
+from gzip import GzipFile
 
 
 class PANDA_dataset(Dataset):
-    def __init__(self, tiff_folder, train_info_path=None, transform=None):
+    def __init__(self, pt_folder, train_info_path=None, transform=None):
         super(Dataset, self).__init__()
         print('Loading dataset...')
         # Load data
         # Store the paths to the .gz file as a dictionary {patientID: complete_path_to_file}
-        self.tiff_paths = {filename.split('.')[0]: os.path.join(tiff_folder, filename) for filename in
-                           os.listdir(tiff_folder)}
+        self.pt_paths = {filename.split('_')[0]: os.path.join(pt_folder, filename.split('_')[0]) for filename in
+                           os.listdir(pt_folder)}
 
         # Check if dataset is for training or for submission
         if train_info_path:
@@ -31,28 +32,12 @@ class PANDA_dataset(Dataset):
                                 'isup_grade': list(train_info.loc[train_info['image_id'] == Id]['isup_grade'])[0],
                                 'gleason_score': list(train_info.loc[train_info['image_id'] == Id]['gleason_score'])[0]
                                 }
-                           for Id in self.tiff_paths.keys()}
+                           for Id in self.pt_paths.keys()}
         else:
             self.labels = None
 
-        # Test code to verify if there are all the labels for each type of data
-        # fnc_keys = list(fnc['Id'])
-        # sbm_keys = list(sbm['Id'])
-        # print(len(gz_keys), len(fnc_keys), len(sbm_keys))
-        # fnc_missing = []
-        # sbm_missing = []
-        # for k in gz_keys:
-        #     if k not in fnc_keys:
-        #         fnc_missing.append(k)
-        #     if k not in sbm_keys:
-        #         sbm_missing.append(k)
-        # print(fnc_missing, sbm_missing)
-        # pass
-
-        # self.mask = np.array(nib.load(mask_path))
-
         # Prepare num_to_id in order to address the indexes required from torch API
-        self.__num_to_id = {i: k for i, k in enumerate(self.tiff_paths.keys())}
+        self.__num_to_id = {i: k for i, k in enumerate(self.pt_paths.keys())}
         # Create reverse order to have control over dataset patients IDs and indexes
         self.id_to_num = {k: i for i, k in self.__num_to_id.items()}
 
@@ -62,24 +47,21 @@ class PANDA_dataset(Dataset):
 
     def __len__(self):
         # Return the length of the dataset
-        return len(self.tiff_paths.keys())
+        return len(self.pt_paths.keys())
 
     def __getitem__(self, item):
         # Get the ID corresponding to the item (an index) that torch is looking for.
         id = self.__num_to_id[item]
-        # Retrieve all information from the Dataset initialization
-        # Keep brain commented until not working on 3D images
-        # brain =
-        # brain = np.frombuffer(zlib.decompress(open(self.gz_paths[ID], 'rb').read()), dtype='float64').reshape(53, 52, 63, 53)
-        # brain = None
 
-        # sbm = self.sbm[ID]
-        multi_scan = skimage.io.MultiImage(self.tiff_paths[id])
-        scan = multi_scan[1]
+        # multi_scan = skimage.io.MultiImage(self.pt_paths[id])
+        # scan = multi_scan[1]
+        with GzipFile(self.pt_paths[id] + '_values.pt.gz') as f:
+            scan_values = torch.load(f)
+        with GzipFile(self.pt_paths[id] + '_indices.pt.gz') as f:
+            scan_indices = torch.load(f)
+        scan_size = torch.load(self.pt_paths[id] + '_size.pt')
 
-        # numero canali come prima dimensione per fare conv2D:
-        # scan = np.swapaxes(scan, 1, 2)
-        # scan = np.swapaxes(scan, 0, 1)
+        scan = torch.sparse_coo_tensor(indices=scan_indices, values=scan_values, size=scan_size, dtype=torch.uint8).to_dense()
 
         # Create sample
         sample = {
@@ -87,8 +69,6 @@ class PANDA_dataset(Dataset):
             # 'sbm': sbm,
             'scan': scan
         }
-        # if self.fnc:
-        #    sample['fnc'] = self.fnc[ID]
         # Add labels to the sample if the dataset is the training one.
         if self.labels:
             sample['label'] = self.labels[id]['isup_grade']
@@ -118,11 +98,10 @@ class Crop:
         for i in range(height // self.crop_dim):
             for j in range(width // self.crop_dim):
                 crop = scan[i * self.crop_dim:(i + 1) * self.crop_dim, j * self.crop_dim:(j + 1) * self.crop_dim, ...]
-                if crop.mean() < self.threshold_mean:
+                if crop.float().mean() > self.threshold_mean:
                     crops.append(crop)
-        # scan = np.stack(crops, axis=-1)
-
-        return {**sample, 'scan': torch.tensor(crops)}
+        crops = torch.stack(crops, dim=0)
+        return {**sample, 'scan': crops}
 
 
 class NormScale:
@@ -136,7 +115,7 @@ class NormScale:
 
     def __call__(self, sample, *args, **kwargs):
         scan = sample['scan']
-        scan: np.ndarray = scan / 255.0
+        scan = scan / 255.0
 
         return {**sample, 'scan': scan.astype('float32')}
 
@@ -166,53 +145,53 @@ class SwapAxes:
         scan = scan.transpose(1, 3)
 
         return {**sample, 'scan': np.array(scan).astype('float32')}
+#
+#
+# # Custom transform example
+# class ToTensor:
+#     def __call__(self, sample):
+#         scan = torch.tensor(sample['scan']).float()
+#         label = torch.tensor(sample['label']['isup_grade']).int()
+#         # Sto togliendo di mezzo le altre informazioni correlate alle labels
+#
+#         return {**sample, 'scan': scan, 'label': label}
 
-
-# Custom transform example
-class ToTensor:
-    def __call__(self, sample):
-        scan = torch.tensor(sample['scan']).float()
-        label = torch.tensor(sample['label']['isup_grade']).int()
-        # Sto togliendo di mezzo le altre informazioni correlate alle labels
-
-        return {**sample, 'scan': scan, 'label': label}
-
-
-def collate_fn(batch, training=False):
-    """
-    Trasforma righe in colonne.
-    [
-        ['ID': ID0, 'scan': scan0, 'label': label0],
-        ['ID': ID1, 'scan': scan1, 'label': label1].
-        ...
-    ]
-    ---------->
-    [
-    'ID': [ID0, ID1, ...],
-    'scan': [scan0, scan1, ...],
-    'label': [label0, label1, ...],
-    ...]
-    """
-    IDs = []
-    labels = []
-    scans = []
-    for a in batch:
-        ID, scan = a['ID'], a['scan']
-        if training:
-            label = a['label']
-            labels.append(label['isup_grade'])
-        IDs.append(ID)
-        scans.append(torch.tensor(scan))
-
-    sample = {
-        'ID': IDs,
-        'scan': scans,
-    }
-
-    if training:
-        sample['label'] = torch.tensor(labels)
-
-    return sample
+#
+# def collate_fn(batch, training=False):
+#     """
+#     Trasforma righe in colonne.
+#     [
+#         ['ID': ID0, 'scan': scan0, 'label': label0],
+#         ['ID': ID1, 'scan': scan1, 'label': label1].
+#         ...
+#     ]
+#     ---------->
+#     [
+#     'ID': [ID0, ID1, ...],
+#     'scan': [scan0, scan1, ...],
+#     'label': [label0, label1, ...],
+#     ...]
+#     """
+#     IDs = []
+#     labels = []
+#     scans = []
+#     for a in batch:
+#         ID, scan = a['ID'], a['scan']
+#         if training:
+#             label = a['label']
+#             labels.append(label['isup_grade'])
+#         IDs.append(ID)
+#         scans.append(torch.tensor(scan))
+#
+#     sample = {
+#         'ID': IDs,
+#         'scan': scans,
+#     }
+#
+#     if training:
+#         sample['label'] = torch.tensor(labels)
+#
+#     return sample
 
 
 class NormCropsNumber:
@@ -242,7 +221,7 @@ if __name__ == '__main__':
     from torchvision import transforms
 
     base_path = os.path.join('..', 'dataset')
-    train_tiff_folder = os.path.join(base_path, 'train_images')
+    train_pt_folder = os.path.join(base_path, 'train_images_torch')
     train_info_path = os.path.join(base_path, 'train.csv')
     mask_path = os.path.join(base_path, 'train_label_masks')
     # mean_path = os.path.join(base_path, 'dataset', 'mean.pt')
@@ -250,9 +229,9 @@ if __name__ == '__main__':
 
     # Define transformations
     # trans = transforms.Compose([Resize((1840, 1728))])
-    trans = transforms.Compose([Crop(256, .95), NormCropsNumber(25)])
-    dataset = PANDA_dataset(train_tiff_folder, transform=trans)
-    dataloader = DataLoader(dataset, batch_size=10, shuffle=False, pin_memory=True, num_workers=0)
+    trans = transforms.Compose([Crop(256, .05), NormCropsNumber(25)])
+    dataset = PANDA_dataset(train_pt_folder, transform=trans)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True, num_workers=0)
 
     crops = []
     for batch in tqdm(dataloader):

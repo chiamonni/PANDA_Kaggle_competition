be82c11b318517d302a353df56c96539828565af
mawanda <mawanda-jun>
2020-06-15 21:26:20 +0200

    new way to extract images and save them into new files

--- decompress_and_convert.py
+++ decompress_and_convert.py
@@ -19,41 +19,104 @@ import os
 import torch
 from tqdm import tqdm
 import gzip
+from PIL import Image
+from welford import Welford
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+os.setgid(1000), os.setuid(1000)
+
+
+def crop_image_only_outside(img, tol=0):
+    # img is 2D or 3D image data
+    # tol  is tolerance
+    mask = img > tol
+    if img.ndim == 3:
+        mask = mask.all(2)
+    m, n = mask.shape
+    mask0, mask1 = mask.any(0), mask.any(1)
+    col_start, col_end = mask0.argmax(), n-mask0[::-1].argmax()
+    row_start, row_end = mask1.argmax(), m-mask1[::-1].argmax()
+    return img[row_start:row_end, col_start:col_end]
+
+
+def save_to_sparse(img, filename, dest_path):
+    # Transform to torch tensor to do operations
+    img = torch.tensor(img, dtype=torch.uint8)
+    # Calculate sparse tensor
+    # Indices
+    indices = torch.nonzero(img, as_tuple=True)
+    # Cast to right data type to spare space
+    values = img[indices].type(torch.uint8)
+    indices = torch.stack(indices, 1).type(torch.int32)
+    size = img.shape
+    # Create new h5 file path
+    new_filepath = os.path.join(dest_path, filename)
+    # (Optional: check if images are reconstructed in the right way)
+    # reconstruct_img = torch.sparse_coo_tensor(indices, values, size, dtype=torch.uint8).to_dense()
+    # assert t_img_negative.equal(reconstruct_img)
+    # torch.save(torch.tensor(img, dtype=torch.uint8), new_filepath + '.pt')
+    # Assign new names for indices, values and size
+    indices_path = new_filepath + '_indices.pt.gz'
+    values_path = new_filepath + '_values.pt.gz'
+    size_path = new_filepath + '_size.pt'
+    # Save the tensors to disk
+    with gzip.GzipFile(indices_path, 'w', compresslevel=1) as f:
+        torch.save(indices, f)
+    with gzip.GzipFile(values_path, 'w', compresslevel=1) as f:
+        torch.save(values, f)
+    torch.save(size, size_path)
+
+
+def save_jpeg(img, filename, dest_path):
+    os.makedirs(dest_path, exist_ok=True)
+    Image.fromarray(img).save(os.path.join(dest_path, filename + '.jpeg'))
+
+
+def save_torch(img, filename, dest_path):
+    os.makedirs(dest_path, exist_ok=True)
+    torch.save(img, os.path.join(dest_path, filename + '.pt'))
+
+
+class ManageDataset(Dataset):
+    def __init__(self, archive_path, paths_list, dest_folder):
+        Dataset.__init__(self)
+        self.archive_path = archive_path
+        self.paths_list = paths_list
+        self.dest_folder = dest_folder
+
+    def __len__(self):
+        return len(self.paths_list)
+
+    def __getitem__(self, item):
+        with ZipFile(self.archive_path, 'r') as zipped_files:
+            file = self.paths_list[item]
+            # Open tiff image and obtain second layer (in the 'asarray' parameter).
+            img = TiffFile(BytesIO(zipped_files.read(file))).asarray(1)
+        # Convert image to negative - to optain more zeros
+        img = 255 - img
+        # Crop image to nonzero borders
+        img: np.ndarray = crop_image_only_outside(img)
+        # Retrieve filename
+        filename = file.filename.split('/')[-1].split('.')[0]
+        # Save sparse version of tensor
+        # save_to_sparse(img, filename, os.path.join(dest_path, 'sparse'))
+        # Save jpeg version of image
+        save_jpeg(img, filename, os.path.join(self.dest_folder, 'type1'))
+        return filename
 
 
 if __name__ == '__main__':
-    path_to_compressed_archive = 'D:\\datasets\\PANDA_dataset\\prostate-cancer-grade-assessment.zip'
-    path_to_dest_dataset = '../dataset/train_images_torch'
-
+    path_to_compressed_archive = '/opt/whole_dataset/prostate-cancer-grade-assessment.zip'
+    path_to_dest_dataset = '/opt/local_dataset/images'
+    paths_list = []
     with ZipFile(path_to_compressed_archive, 'r') as zipped_files:  # Open zipped file
         for file in tqdm(zipped_files.infolist()):  # Obtain file list
             zip_filepath = file.filename  # Extract actual file path
             if 'train_images' in zip_filepath and zip_filepath.endswith('.tiff'):  # Leave masks
-                # Open tiff image and obtain second layer (in the 'asarray' parameter)
-                img = TiffFile(BytesIO(zipped_files.read(file))).asarray(1)
-                # Transform to torch tensor to do operations
-                t_img = torch.tensor(img, dtype=torch.uint8)
-                # Calculate its opposite to cast the images to sparse representation
-                t_img_negative = 255 - t_img
-                # Calculate sparse tensor
-                t_img_negative_sparse = t_img_negative.to_sparse()
-                # Cast to right data type to spare space
-                indices = t_img_negative_sparse.indices().type(torch.int16)
-                values = t_img_negative_sparse.values().type(torch.uint8)
-                size = t_img_negative_sparse.size()
-                # Create new h5 file path
-                new_filepath = os.path.join(path_to_dest_dataset, file.filename.split('/')[-1].split('.')[0])
-                # (Optional: check if images are reconstructed in the right way)
-                # reconstruct_img = torch.sparse_coo_tensor(indices, values, size, dtype=torch.uint8).to_dense()
-                # assert t_img_negative.equal(reconstruct_img)
-                # torch.save(torch.tensor(img, dtype=torch.uint8), new_filepath + '.pt')
-                # Assign new names for indices, values and size
-                indices_path = new_filepath + '_indices.pt.gz'
-                values_path = new_filepath + '_values.pt.gz'
-                size_path = new_filepath + '_size.pt'
-                # Save the tensors to disk
-                with gzip.GzipFile(indices_path, 'w', compresslevel=1) as f:
-                    torch.save(indices, f)
-                with gzip.GzipFile(values_path, 'w', compresslevel=1) as f:
-                    torch.save(values, f)
-                torch.save(size, size_path)
+                paths_list.append(file)
+
+    dataset = ManageDataset(path_to_compressed_archive, paths_list, path_to_dest_dataset)
+    loader = DataLoader(dataset, num_workers=12)
+    for _ in tqdm(loader, desc='Processing images'):
+        pass

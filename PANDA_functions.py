5c7ed7b9cbaa42fb535f831013d4e4c163d1be4b
mawanda <mawanda-jun>
2020-06-17 16:35:55 +0200

    Now qwk function works correctly

--- PANDA_functions.py
+++ PANDA_functions.py
@@ -1,53 +1,63 @@
 import torch
 from torch.nn import functional as F
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-def _quadratic_kappa_coefficient(y_true, y_pred):
-    y_true, y_pred = y_true.squeeze(), y_pred.squeeze()
-    y_true = y_true.type(torch.float32)
-    n_classes = y_pred.shape[-1]
-    weights = torch.arange(0, n_classes, dtype=torch.float32, device=DEVICE) / (n_classes - 1)
+
+def _quadratic_kappa_coefficient(output, target):
+    output, target = output.type(torch.float32), target.type(torch.float32)
+    n_classes = target.shape[-1]
+    weights = torch.arange(0, n_classes, dtype=torch.float32, device=output.device) / (n_classes - 1)
     weights = (weights - torch.unsqueeze(weights, -1)) ** 2
 
-    hist_true = torch.sum(y_true, dim=0)
-    hist_pred = torch.sum(y_pred, dim=0)
+    C = (output.t() @ target).t()  # confusion matrix
 
-    E = torch.unsqueeze(hist_true, dim=-1) * hist_pred
-    E = E / torch.sum(E)
+    hist_true = torch.sum(target, dim=0).unsqueeze(-1)
+    hist_pred = torch.sum(output, dim=0).unsqueeze(-1)
 
-    O = (y_true.t() @ y_pred).t()  # confusion matrix
-    O = O / torch.sum(O)
+    E = hist_true @ hist_pred.t()  # Outer product of histograms
+    E = E / C.sum() # Normalize to the sum of C.
 
-    num = weights * O
+    num = weights * C
     den = weights * E
 
-    QWK = (1 - torch.sum(num) / torch.sum(den))
+    QWK = 1 - torch.sum(num) / torch.sum(den)
     return QWK
 
 
-def _quadratic_kappa_loss(y_true, y_pred, scale=2.0):
-    QWK = _quadratic_kappa_coefficient(y_true, y_pred)
+def _quadratic_kappa_loss(output, target, scale=2.0):
+    QWK = _quadratic_kappa_coefficient(output, target)
     loss = -torch.log(torch.sigmoid(scale * QWK))
     return loss
 
 
 class QWKMetric(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, binned=False):
         super().__init__()
+        self.binned = binned
 
     def forward(self, output, target):
-        target = F.one_hot(target.to(torch.int64).squeeze(), num_classes=6).to(DEVICE)
-        return _quadratic_kappa_coefficient(target, output)
+        target = F.one_hot(target.squeeze(), num_classes=6).to(target.device)
+        if self.binned:
+            output = torch.sigmoid(output).sum(1).round().long()
+            output = F.one_hot(output.squeeze(), num_classes=6).to(output.device)
+        else:
+            output = torch.softmax(output, dim=1)
+        return _quadratic_kappa_coefficient(output, target)
 
 
 class QWKLoss(torch.nn.Module):
-    def __init__(self, scale=2.0):
+    def __init__(self, scale=2.0, binned=False):
         super().__init__()
+        self.binned = binned
         self.scale = scale
 
     def forward(self, output, target):
-        target = F.one_hot(target.to(torch.int64).squeeze(), num_classes=6).to(DEVICE)
-        return _quadratic_kappa_loss(target, output, self.scale)
+        target = F.one_hot(target.squeeze(), num_classes=6).to(target.device)
+        if self.binned:
+            output = torch.sigmoid(output).sum(1).round().long()
+            output = F.one_hot(output.squeeze(), num_classes=6).to(output.device)
+        else:
+            output = torch.softmax(output, dim=1)
+        return _quadratic_kappa_loss(output, target, self.scale)
 
 '''
 if __name__ == '__main__':
